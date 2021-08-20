e4a86b0b71a0701a98aca78647a49d2ddcc49715
GwoonGwen <74727367+GwoonGwen@users.noreply.github.com>
2021-08-20 17:54:56 +0200

    adjusted css forecast margin

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,15 +2,33 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+    function maxTemp() {
+        let temperature = Math.round(props.data.temp.max);
+        return `${temperature}`;
+    }
+    function minTemp() {
+        let temperature = Math.round(props.data.temp.min);
+        return `${temperature}`;
+    }
+
+    function day() {
+        let date = new Date(props.data.dt * 1000);
+        let day = date.getDay();
+
+        let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+        return days[day];
+    }
+
     return (
     <div>
-        <div className="WeatherForecast-day">{props.data.dt}</div>
+        <div className="WeatherForecast-day">{day()}</div>
             <div className="WeatherIcon-FC">
-                <WeatherIcon code={props.data.weather[0].icon} size={36} /></div>
+                <WeatherIcon code={props.data.weather[0].icon} size={45} /></div>
             <div className="WeatherForecast-temp">
-                <span className="WeatherForecast-max">{props.data.temp.max}</span>
-                <span className="WeatherForecast-min">{props.data.temp.min}</span>
+                <span className="WeatherForecast-max">{minTemp()}º</span>
+                <span className="WeatherForecast-min">{maxTemp()}º</span>
+            </div>
         </div>
-    </div>
     );
 }
