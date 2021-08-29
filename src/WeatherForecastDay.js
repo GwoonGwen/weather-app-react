4e9403d69abcd9d34ffb7d02a5895817f3e4d545
GwoonGwen <74727367+GwoonGwen@users.noreply.github.com>
2021-08-29 12:38:46 +0200

    final size, name and margin change forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -26,7 +26,7 @@ export default function WeatherForecastDay(props) {
     <div>
         <div className="WeatherForecast-day">{day()}</div>
             <div className="WeatherIcon-FC">
-                <WeatherIcon code={props.data.weather[0].icon} size={55} /></div>
+                <WeatherIcon code={props.data.weather[0].icon} size={50} /></div>
             <div className="WeatherForecast-temp">
                 <span className="WeatherForecast-max">{minTemp()}º</span>
                 <span className="WeatherForecast-min">{maxTemp()}º</span>
